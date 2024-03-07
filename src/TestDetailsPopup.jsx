c6336e308d066cfdfb1bb7aefff35373af02934e
GroundMobil <groundmobil@gmail.com>
2024-03-07 22:15:52 +0530

    desc

--- src/TestDetailsPopup.jsx
+++ src/TestDetailsPopup.jsx
@@ -4,6 +4,7 @@ import LabDetailsPopup from './LabDetailsPopup';
 const TestDetailsPopup = ({ onClose }) => {
   const [addedToCart, setAddedToCart] = useState(Array(12).fill(false));
   const [showLabDetails, setShowLabDetails] = useState(false);
+  const [expandedDescriptionIndex, setExpandedDescriptionIndex] = useState(null);
 
   const handleAddToCart = (index) => {
     setAddedToCart((prev) => {
@@ -19,16 +20,66 @@ const TestDetailsPopup = ({ onClose }) => {
 
   const closeLabDetails = () => {
     setShowLabDetails(false);
+    setExpandedDescriptionIndex(null); 
   };
+
+  const toggleDescription = (index) => {
+    setExpandedDescriptionIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  
+  const getDescriptionPopup = (test) => (
+    <div
+      style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(/Images/dr.png)`,
+        backgroundSize: 'cover',  // Set background image to cover the entire div
+        backgroundPosition: 'center', // Center the background image
+        width: '100%', // Set the width to cover the entire screen
+        height: '100vh', // Set the height to cover the entire screen
+        padding: '20px',
+        borderRadius: '18px',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        zIndex: '9999',
+        color: 'white',
+        display: 'flex', // Add flex display to center the content
+      flexDirection: 'column', // Stack children vertically
+      alignItems: 'center', // Center horizontally
+      justifyContent: 'center', // Center vertically
+      textAlign: 'center', 
+      }}
+    >
+      <button
+        onClick={() => setExpandedDescriptionIndex(null)}
+        style={{
+          background: 'transparent',
+          border: 'none',
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+          cursor: 'pointer',
+          fontSize: '16px',
+          color: 'white',
+        }}
+      >
+        &times;
+      </button>
+      <h4 style={{ fontSize: '44px' }}>{test.name}</h4> {/* Adjust the font size */}
+    <p style={{ fontSize: '25px' }}>{test.description}</p> {/* Adjust the font size */}
+    </div>
+  );
 
   const isAnyTestSelected = addedToCart.some((isSelected) => isSelected);
 
   // Test data for each grid square
   const testData = [
-    { name: 'HBA1c', description: 'Description for Test 1' },
-    { name: 'Complete Blood Count(CBC)- EDTA Whole Blood', description: 'Description for Test 2' },
+    { name: 'HBA1c', description: 'This is a blood test which shows a 3 months average sugar level in blood. Heamoglobin A1c is a type of protein in red blood cell that is resposible for exchange of gases, oxygen to the cell and brings back carbon dioxide. When sugar builds in the blood it binds to Heamoglobin. This test determines how much sugar is bound to RBC. Since RBC lives for 3 months it shows a 3 months average. This test helps ypur doctor understand the effectiveness of the sugar control regime he/she has prescribed and adjust (if required) accordingly.' },
+    { name: 'Complete Blood Count- EDTA Blood', description: 'Description for Test 2' },
     { name: 'Glucose Post Prandial (PPBS)', description: 'Description for Test 2' },
-    { name: 'Glucose Random(RBS)- Sodium Fluoride', description: 'Description for Test 2' },
+    { name: 'Glucose Random(RBS)', description: 'Description for Test 2' },
     { name: 'Liver Function Test (LFT)', description: 'Description for Test 2' },
     { name: 'Glucose, Fluid', description: 'Description for Test 2' },
     { name: 'Diabetes Profile, Basic', description: 'Description for Test 2' },
@@ -36,7 +87,7 @@ const TestDetailsPopup = ({ onClose }) => {
     { name: 'Glucose Fasting(FBS)- Sodium Fluoride', description: 'Description for Test 2' },
     { name: 'Stool Examination', description: 'Description for Test 2' },
     { name: 'Lipid Profile- Serum', description: 'Description for Test 2' },
-    { name: 'Complete Urine Examination(CUE)- Spot Urine', description: 'Description for Test 2' },
+    { name: 'Complete Urine Examination', description: 'Description for Test 2' },
   ];
 
   return (
@@ -44,11 +95,13 @@ const TestDetailsPopup = ({ onClose }) => {
       {showLabDetails ? (
         <LabDetailsPopup onClose={closeLabDetails} />
       ) : (
-        <div
-          style={{
-            position: "fixed", left: "50%", top: "55%", transform: "translate(-50%, -50%)", borderRadius: "10px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)", display: "grid", gridTemplateColumns: "repeat(4, minmax(15vw, 1fr))", gap: "20px", padding: "15px", background: "transparent",
-          }}
-        >
+       <div>
+          <div
+            className={expandedDescriptionIndex !== null ? 'grid-hidden' : 'grid-visible'}
+            style={{
+              position: "fixed", left: "50%", top: "55%", transform: "translate(-50%, -50%)", borderRadius: "10px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)", display: "grid", gridTemplateColumns: "repeat(4, minmax(15vw, 1fr))", gap: "20px", padding: "15px", background: "transparent",
+            }}
+          >
           {testData.map((test, index) => (
             <div
               key={index}
@@ -56,10 +109,24 @@ const TestDetailsPopup = ({ onClose }) => {
                 border: "1px solid #ccc", borderRadius: "8px", width: "13vw", height: "15vw", textAlign: "center", background: "#fff", display: "flex", flexDirection: "column", justifyContent: "space-between", padding: "3px", marginBottom: "20px",
               }}
             >
-              <div>
-                
+               <div>
                 <h4>{test.name}</h4>
-                <p>{test.description}</p>
+                <p>
+                  {`${test.description.slice(0, 15)}...`}
+                  {test.description.length > 15 && (
+                    <span
+                      style={{
+                        color: 'blue',
+                        textDecoration: 'underline',
+                        cursor: 'pointer',
+                        marginLeft: '5px',
+                      }}
+                      onClick={() => setExpandedDescriptionIndex(index)}
+                    >
+                      Show more
+                    </span>
+                  )}
+                </p>
               </div>
               <button
                 onClick={() => handleAddToCart(index)}
@@ -71,6 +138,8 @@ const TestDetailsPopup = ({ onClose }) => {
               </button>
             </div>
           ))}
+            {expandedDescriptionIndex !== null &&
+            getDescriptionPopup(testData[expandedDescriptionIndex])}
           <button
             onClick={openLabDetails}
             disabled={!isAnyTestSelected}
@@ -88,6 +157,7 @@ const TestDetailsPopup = ({ onClose }) => {
           >
             Back
           </button>
+        </div>
         </div>
       )}
     </div>
