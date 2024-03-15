aabdb0fd3ce008d31d898bc3274bef21b991d650
GroundMobil <groundmobil@gmail.com>
2024-03-15 13:41:43 +0530

    booknow

--- src/BookNow.jsx
+++ src/BookNow.jsx
@@ -2,11 +2,13 @@ import React, { useRef, useState } from "react";
 import { FaMapMarkerAlt, FaSearch, FaFileSignature } from "react-icons/fa";
 import TestDetailsPopup from './TestDetailsPopup';
 
+
 const BookNow = () => {
   const fileInputRef = useRef(null);
   const [pincode, setPincode] = useState("");
   const [selectedTest, setSelectedTest] = useState("");
-  const [showPopup, setShowPopup] = useState(false);
+  const [searchTest, setSearchTest] = useState("");
+  const [showPopup, setShowPopup] = useState(false); 
   const [isSearchEnabled, setIsSearchEnabled] = useState(false);
 
   const handleFileUpload = () => {
@@ -17,18 +19,17 @@ const BookNow = () => {
 
   const handleFileChange = async (event) => {
     const selectedFile = event.target.files[0];
-    console.log("Selected File:", selectedFile);
-
+  
     try {
       const formData = new FormData();
       formData.append('file', selectedFile);
-
+  
       // Make a separate API call to handle file upload
       const fileUploadResponse = await fetch('http://localhost:8080/upload', {
         method: 'POST',
         body: formData,
       });
-
+  
       console.log('File upload response:', fileUploadResponse);
     } catch (error) {
       console.error('Error uploading file:', error.message);
@@ -38,13 +39,13 @@ const BookNow = () => {
   const handleSearch = async () => {
     try {
       const allowedPincodes = ['411017', '444606'];
-
+  
       if (allowedPincodes.includes(pincode)) {
         if (selectedTest.toLowerCase() === 'diabetes') {
           setShowPopup(true);
           console.log("Pin Code:", pincode);
           console.log("Selected Test:", selectedTest);
-
+  
           // Make an API call to store data in the database
           await fetch('http://localhost:8080/booknow', {
             method: 'POST',
@@ -53,7 +54,7 @@ const BookNow = () => {
             },
             body: JSON.stringify({ pincode, selectedTest }),
           });
-
+  
         } else {
           // Display a message if the entered test is not "diabetes"
           alert('This test is not availabe currently.');
@@ -94,189 +95,190 @@ const BookNow = () => {
   return (
     <div>
       {showPopup ? (
-        <TestDetailsPopup onClose={closePopup} />
-      ) : (
-        <div
+  <TestDetailsPopup onClose={closePopup} pincode={pincode} />
+) : (
+  <div
+  style={{
+    textAlign: "center",
+    color: "white",
+    marginTop: "25vh",
+    transform: "translateY(-50%)",
+    fontSize: "24px",
+    fontWeight: "30",
+    fontFamily: "sans-serif",
+  }}
+>
+  <h1>Book Any Test From Any Lab</h1>
+
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: "20px",
+    }}
+  >
+    <div className="mobile-layout">
+      <div style={{ position: "relative" }}>
+        <FaMapMarkerAlt
+          className="icons"
           style={{
-            textAlign: "center",
-            color: "white",
-            marginTop: "25vh",
+            position: "absolute",
+            left: "10px",
+            top: "50%",
             transform: "translateY(-50%)",
-            fontSize: "24px",
-            fontWeight: "30",
-            fontFamily: "sans-serif",
+            color: "black",
           }}
-        >
-          <h1>Book Any Test From Any Lab</h1>
+        />
+        <input
+          type="text"
+          placeholder="Enter Pincode"
+          value={pincode}
+          onChange={handlePincodeChange}
+          style={{
+            marginBottom: "10px",
+            padding: "10px",
+            fontSize: "16px",
+            borderRadius: "70px",
+            border: "1px solid #ccc",
+            paddingLeft: "40px",
+          }}
+        />
+      </div>
 
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              marginTop: "20px",
-            }}
-          >
-            <div className="mobile-layout">
-              <div style={{ position: "relative" }}>
-                <FaMapMarkerAlt
-                  className="icons"
-                  style={{
-                    position: "absolute",
-                    left: "10px",
-                    top: "50%",
-                    transform: "translateY(-50%)",
-                    color: "black",
-                  }}
-                />
-                <input
-                  type="text"
-                  placeholder="Enter Pincode"
-                  value={pincode}
-                  onChange={handlePincodeChange}
-                  style={{
-                    marginBottom: "10px",
-                    padding: "10px",
-                    fontSize: "16px",
-                    borderRadius: "70px",
-                    border: "1px solid #ccc",
-                    paddingLeft: "40px",
-                  }}
-                />
-              </div>
-
-              <div style={{ position: "relative" }}>
-                <FaSearch
-                  className="icons"
-                  style={{
-                    position: "absolute",
-                    left: "10px",
-                    top: "50%",
-                    transform: "translateY(-50%)",
-                    color: "black",
-                  }}
-                />
-                <input
-                  type="text"
-                  placeholder="Select Test"
-                  value={selectedTest}
-                  onChange={handleSearchTestChange}
-                  style={{
-                    padding: "10px",
-                    fontSize: "16px",
-                    borderRadius: "70px",
-                    border: "1px solid #ccc",
-                    paddingLeft: "40px",
-                  }}
-                />
-              </div>
-            </div>
-
-            <div className="desktop-tab-layout">
-              <div style={{ display: "flex", marginBottom: "30px" }}>
-                <div style={{ position: "relative" }}>
-                  <FaMapMarkerAlt
-                    className="icons"
-                    style={{
-                      position: "absolute",
-                      left: "10px",
-                      top: "50%",
-                      transform: "translateY(-50%)",
-                      color: "black",
-                    }}
-                  />
-                  <input
-                    type="text"
-                    placeholder="Enter Pincode"
-                    value={pincode}
-                    onChange={handlePincodeChange}
-                    style={{
-                      marginRight: "10px",
-                      padding: "10px",
-                      fontSize: "16px",
-                      borderRadius: "70px",
-                      border: "1px solid #ccc",
-                      paddingLeft: "40px",
-                    }}
-                  />
-                </div>
-
-                <div style={{ position: "relative" }}>
-                  <FaSearch
-                    className="icons"
-                    style={{
-                      position: "absolute",
-                      left: "10px",
-                      top: "50%",
-                      transform: "translateY(-50%)",
-                      color: "black",
-                    }}
-                  />
-                  <input
-                    type="text"
-                    placeholder="Select Test"
-                    value={selectedTest}
-                    onChange={handleSearchTestChange}
-                    style={{
-                      padding: "10px",
-                      fontSize: "16px",
-                      borderRadius: "70px",
-                      border: "1px solid #ccc",
-                      paddingLeft: "40px",
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <button
-              onClick={handleSearch}
-              disabled={!isSearchEnabled}
-              style={{
-                padding: "10px 20px",
-                fontSize: "18px",
-                backgroundColor: "#007BFF",
-                color: "white",
-                borderRadius: "70px",
-                cursor: isSearchEnabled ? "pointer" : "not-allowed", // Change cursor style
-                marginTop: "20px",
-              }}
-            >
-              Search
-            </button>
-            <div>or</div>
-            <button
-              onClick={handleFileUpload}
-              style={{
-                padding: "10px 20px",
-                fontSize: "18px",
-                backgroundColor: "#007BFF",
-                color: "white",
-                borderRadius: "70px",
-                cursor: "pointer",
-              }}
-            >
-              <FaFileSignature
-                className="icons"
-                style={{ color: "black", fontSize: "15px" }}
-              />{" "}
-              Upload Prescription
-            </button>
-
-            <input
-              ref={fileInputRef}
-              type="file"
-              style={{ display: "none" }}
-              onChange={handleFileChange}
-            />
-          </div>
-        </div>
-      )}
+      <div style={{ position: "relative" }}>
+        <FaSearch
+          className="icons"
+          style={{
+            position: "absolute",
+            left: "10px",
+            top: "50%",
+            transform: "translateY(-50%)",
+            color: "black",
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Select Test"
+          value={selectedTest}
+          onChange={handleSearchTestChange}
+          style={{
+            padding: "10px",
+            fontSize: "16px",
+            borderRadius: "70px",
+            border: "1px solid #ccc",
+            paddingLeft: "40px",
+          }}
+        />
+      </div>
     </div>
-  );
+
+    <div className="desktop-tab-layout">
+      <div style={{ display: "flex", marginBottom: "30px" }}>
+        <div style={{ position: "relative" }}>
+          <FaMapMarkerAlt
+            className="icons"
+            style={{
+              position: "absolute",
+              left: "10px",
+              top: "50%",
+              transform: "translateY(-50%)",
+              color: "black",
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter Pincode"
+            value={pincode}
+            onChange={handlePincodeChange}
+            style={{
+              marginRight: "10px",
+              padding: "10px",
+              fontSize: "16px",
+              borderRadius: "70px",
+              border: "1px solid #ccc",
+              paddingLeft: "40px",
+            }}
+          />
+        </div>
+
+        <div style={{ position: "relative" }}>
+          <FaSearch
+            className="icons"
+            style={{
+              position: "absolute",
+              left: "10px",
+              top: "50%",
+              transform: "translateY(-50%)",
+              color: "black",
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Select Test"
+            value={selectedTest}
+            onChange={handleSearchTestChange}
+            style={{
+              padding: "10px",
+              fontSize: "16px",
+              borderRadius: "70px",
+              border: "1px solid #ccc",
+              paddingLeft: "40px",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+
+    <button
+      onClick={handleSearch}
+      disabled={!isSearchEnabled}
+      style={{
+        padding: "10px 20px",
+        fontSize: "18px",
+        backgroundColor: "#007BFF",
+        color: "white",
+        borderRadius: "70px",
+        cursor: isSearchEnabled ? "pointer" : "not-allowed", // Change cursor style
+        marginTop: "20px",
+      }}
+    >
+      Search
+    </button>
+    <div>or</div>
+    <button
+      onClick={handleFileUpload}
+      style={{
+        padding: "10px 20px",
+        fontSize: "18px",
+        backgroundColor: "#007BFF",
+        color: "white",
+        borderRadius: "70px",
+        cursor: "pointer",
+      }}
+    >
+      <FaFileSignature
+        className="icons"
+        style={{ color: "black", fontSize: "15px" }}
+      />{" "}
+      Upload Prescription
+    </button>
+
+    <input
+      ref={fileInputRef}
+      type="file"
+      style={{ display: "none" }}
+      onChange={handleFileChange}
+    />
+  </div>
+</div>
+)}
+</div>
+);
 };
 
 export default BookNow;
+
 
 // CSS
 const styles = `
